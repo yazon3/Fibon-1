e711619607d39d217db847a227a35738fea0788d
Piotr Gankiewicz <spetz90@gmail.com>
2017-06-24 10:25:16 +0200

    Endpoints

---
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public async Task<IActionResult> Get(int number)
+        {
+            await Task.CompletedTask;
+            return Content(number.ToString());
+        }
+
+		[HttpPost("{number}")]
+		public async Task<IActionResult> Post(int number)
+		{
+            await Task.CompletedTask;
+            return Accepted($"fibonacci/{number}", null);
+		}
+    }
+}
---
+++ src/Fibon.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Content("Hello from Fibon API.");
+        }
+    }
+}
